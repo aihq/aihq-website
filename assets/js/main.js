1e21793a1bac0acd07469f2680c1b386fa9cf5eb
Milan Ferus-Comelo <mferus-comelo@outlook.com>
2020-04-13 19:29:10 -0700

    updated design made it more template friendly created table views for conferences and camps pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,39 @@
 $(window).scroll(function() {
-    if($(this).scrollTop() > ($('.site-header').height())) {
-        $('.site-header').addClass('opaque');
+    if($(this).scrollTop() > ($('#site-header').height())) {
+        $('#site-header').addClass('opaque');
 
-        $('.site-title').removeClass('logo-white');
-        $('.site-title').addClass('logo-black');
+        $('#logo').attr('src', '/aihq-website/assets/img/logo-black.png');
     } 
     else {
-        $('.site-header').removeClass('opaque');
+        $('#site-header').removeClass('opaque');
 
-        $('.site-title').removeClass('logo-black');
-        $('.site-title').addClass('logo-white');
+        $('#logo').attr('src', '/aihq-website/assets/img/logo-white.png');
     }
 });
+
+function openModal(websiteName){
+    document.getElementsByClassName('bg-modal')[0].style.display = 'flex';
+    document.getElementById('modal-' + websiteName).style.display = 'flex';
+    document.getElementsByTagName('body')[0].style.overflow = 'hidden';
+}
+
+function closeModal(websiteName){
+    document.getElementsByClassName('bg-modal')[0].style.display = 'none';
+    document.getElementById('modal-' + websiteName).style.display = 'none';
+    document.getElementsByTagName('body')[0].style.overflow = 'auto';
+}
+
+var acc = document.getElementsByClassName('course-name');
+var i;
+
+for(i = 0; i < acc.length; i++){
+    acc[i].addEventListener('click', function(){
+        this.classList.toggle('active');
+        var panel = this.nextElementSibling;
+        if(panel.style.maxHeight){
+            panel.style.maxHeight = null;
+        } else{
+            panel.style.maxHeight = panel.scrollHeight + 'px';
+        }
+    });
+}
--- camps.md
+++
@@ -1,6 +0,0 @@
----
-layout: page
-title: Camps
-permalink: /camps/
-hero-title: Camps
----
