3ed5fd9199f468d8fe5d54f1baa53d9bee60e23e
Milan Ferus-Comelo <mferus-comelo@outlook.com>
2020-05-25 15:13:14 -0700

    Created a filter option for camps and conferences

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,6 @@
+camps = [];
+conferences = [];
+
 $(window).scroll(function() {
     if($(this).scrollTop() > ($('#site-header').height())) {
         $('#site-header').addClass('opaque');
@@ -54,6 +57,91 @@ for(i = 0; i < acc.length; i++){
     });
 }
 
-function sortConferences(){
+$('.dropdown-el').click(function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    $(this).toggleClass('expanded');
+    $('#'+$(e.target).attr('for')).prop('checked',true);
+});
+
+$(document).ready(function() {
+    var pathName = window.location.pathname.toString();
+
+    if(pathName.includes('camps') || pathName.includes('conferences')) {
+        var type = pathName.substring(1, pathName.length - 1);
+    
+        var fileUrl = '/assets/csv/' + type + '.csv';
+    
+        $.ajax({
+            type: 'GET',
+            url: fileUrl,
+            dataType: 'text',
+            success: function(csv) {processCSV(csv, type);}
+        });
+    }
+
+    $('.dropdown-el').removeClass('expanded');
+});
+
+function processCSV(csv, type) {
+    if(type == 'conferences') {
+        conferences = $.csv.toObjects(csv);  
+        filterEvents('conferences');
+    } else if(type == 'camps') {
+        camps = $.csv.toObjects(csv);  
+        filterEvents('camps');
+    }
+
+}
+
+function filterEvents(type){
     var filterKeywords = document.getElementById('filterKeywords').value;
+    
+    var radioButtons = document.querySelectorAll('input[name="sortType"]');
+    var sortBy;
+    for(radioButton of radioButtons) {
+        if(radioButton.checked) {
+            sortBy = radioButton.value;
+            break;
+        }
+    }
+    // console.log(sortBy);
+    
+    var filteredEvents;
+
+    if(type == 'conferences') {
+        filteredEvents = conferences.filter(conference => conference.name.toString().includes(filterKeywords));
+    } else if(type == 'camps') {
+        filteredEvents = camps.filter(camps => camps.name.toString().includes(filterKeywords));
+    }
+
+    switch(sortBy) {
+        case 'alphabetical':
+            filteredEvents.sort((a, b) => (a.name > b.name) ? 1 : -1);
+            break;
+        case 'newest':
+            filteredEvents.reverse();
+            break;
+    }
+
+    fillEvents(filteredEvents, type)
+}
+
+function fillEvents(events, type) {
+    // console.log(type);
+
+    var html = '';
+    if(type == 'conferences') {
+        for(i = 0; i < events.length; i++) {
+            var conference = events[i];
+            html += '<div class="conference"><h4><a href="' + conference.link + '" target="_blank">' + conference.name + '</a></h4><p class="conference-location"><i class="fas fa-map-marker-alt"></i>' + conference.location + '</p><p class="conference-date"><i class="fas fa-calendar-day"></i>' + conference.date + '</p><p class="conference-description">' + conference.description + '</p></div>';
+        }
+    } else if(type == 'camps') {
+        for(i = 0; i < events.length; i++) {
+            var camp = events[i];
+            html += '<div class="camp"><h4><a href="' + camp.link + '" target="_blank">' + camp.name + '</a></h4><p class="camp-location"><i class="fas fa-map-marker-alt"></i>' + camp.location + '</p><p class="camp-date"><i class="fas fa-calendar-day"></i>' + camp.date + '</p><p class="camp-description">' + camp.description + '</p></div>';
+        }
+    }
+
+    document.getElementById(type).innerHTML = html;
 }
